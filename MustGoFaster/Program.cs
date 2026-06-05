07fe7abb5d8961b6c58874fb4e1f1c20983af807
Zayveeuhr <daelynxwinston@gmail.com>
2026-06-04 20:11:23 -0400

    Finsiehd level 8

---
+++ MustGoFaster/Program.cs
@@ -0,0 +1,15 @@
+﻿int iChannel = 0;
+
+Console.Write("\nEnter the secret symbol: ");
+string sSecretSymbol = Console.ReadLine()!;
+
+int iChannelOut = sSecretSymbol switch
+{
+    "x" => iChannel = 2,
+    "o" => iChannel = 3,
+    "^" => iChannel = 1,
+    "#" => iChannel = 4,
+    _   => iChannel = 0
+};
+
+Console.WriteLine($"\nThe channel number you have choosen is {iChannel}\n");
--- Reset/Program.cs
+++ Reset/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int iChannel = 0;
+
+Console.Write("\nEnter the secret symbol: ");
+string sSecretSymbol = Console.ReadLine()!;
+
+switch (sSecretSymbol)
+{
+    case "x":
+        iChannel = 2;
+        break;
+    case "o":
+        iChannel = 3;
+        break;
+    case "^":
+        iChannel = 1;
+        break;
+    case "#":
+        iChannel = 4;
+        break;
+    default:
+        iChannel = 0;
+        break;
+}
+
+Console.WriteLine($"\nThe channel number you have choosen is {iChannel}\n");
