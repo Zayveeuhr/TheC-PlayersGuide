c5798b41eac53eb00daae6d6af8da5bdf1566a79
Zayveeuhr <daelynxwinston@gmail.com>
2026-06-09 07:01:38 -0400

    Continuing with chapter 9

---
+++ AnInterpolationExperiment/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+Console.Write("Give me the first number: ");
+int iFirstNumber = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter second number: ");
+int iSecondNumber = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"\nThe number {iFirstNumber}+{iSecondNumber}={iFirstNumber + iSecondNumber}\n");
---
+++ CombinationLock/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Type the 3 digit name: ");
+string name = Console.ReadLine()!;
+
+if (name.Length == 3)
+{
+    if (char.IsDigit(name[0]) && char.IsDigit(name[1]) && char.IsDigit(name[2]))
+    {
+        Console.WriteLine(name);
+    }
+    else
+    {
+        Console.WriteLine("Those were not digits.");
+    }
+}
+else
+{
+    Console.WriteLine("Name is to long.");
+}
